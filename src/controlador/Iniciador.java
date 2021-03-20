fee6cf7105ec18002b5ce147b36da0da1a8d8096
SrJowy <joelbraortiz@gmail.com>
2021-03-20 10:54:51 +0100

    Carga funciona correctamente, añadido Iniciador para realizar pruebas

---
+++ src/controlador/Iniciador.java
@@ -0,0 +1,38 @@
+package controlador;
+
+import java.util.Scanner;
+
+public class Iniciador {
+	public static void main(String[] args) {
+		Iniciador i = new Iniciador();
+		System.out.println("buenas tardes");
+		i.menu();
+		
+	}
+	
+	public Iniciador() {}
+	
+	public void menu() {
+		int i;
+		do {
+			Scanner s = new Scanner(System.in);
+			i = s.nextInt();
+		
+			switch (i) {
+				case 1: {
+					ListaSudokus.getListaSudokus();
+					int[][] a = ListaSudokus.getListaSudokus().getLSudokus();
+					int j = 0;
+					while (j<a.length) {
+						int k = 0;
+						while (k<a[0].length) {
+							System.out.println(a[j][k]);
+							k++;
+						}
+						j++;
+					}
+				}
+			}
+		} while (i!=0);
+	}
+}
--- src/controlador/ListaSudokus.java
+++ src/controlador/ListaSudokus.java
@@ -9,31 +9,52 @@ import java.util.Scanner;
 public class ListaSudokus {
 	private ArrayList<int[][]> lSudokus;
 	private ArrayList<int[][]> lSoluciones;
+	private static ListaSudokus miListaSudokus;
 	
-	public ListaSudokus() {
-		try {
-			Scanner entrada = new Scanner (new FileReader("sudoku.txt"));
-			String val;
-			int[][] a = null;
-			int l = 0;
-			while (entrada.hasNext()) {
-				val = entrada.nextLine();
-				if (val.length() == 9) {
-						a = new int[9][9];
-						for (int i = 0; i < val.length(); i++) {
-							a[l][i] = val.charAt(i);
-							if (l < 9) {
-								lSudokus.add(a);
-							} else if (l > 17) {
-								l = 0;
-							} else {
-								lSoluciones.add(a);
-							}
-						}
-					l++;
+	public ListaSudokus() { //No se si sería recomendable hacer un método exclusivo para la carga
+		lSudokus = new ArrayList<int[][]>();
+		lSoluciones = new ArrayList<int[][]>();
+	try {
+		Scanner entrada = new Scanner (new FileReader("sudoku.txt"));
+		String val;
+		int[][] a = null;
+		int l = 0;
+		while (entrada.hasNext()) {
+			val = entrada.nextLine();
+			if (val.length() == 9) {
+				a = new int[9][9];
+				int i = 0;
+				while (i<a.length) {
+					l = 0;
+					while ( l< a[0].length) {
+						int v = Integer.parseInt(String.valueOf(val.charAt(l)));
+						a[i][l] = v;
+						l ++;
+					}
+					i++;
+					if (i!=9) val = entrada.nextLine();
 				}
 			}
-			entrada.close();
-		} catch (IOException e) {e.printStackTrace();}
+			if (l == 9) {
+				if (lSudokus.size() == lSoluciones.size())
+					lSudokus.add(a);
+				else
+					lSoluciones.add(a);
+				l = 0;
+			}
+				
+			
+		}
+		entrada.close();
+	} catch (IOException e) {e.printStackTrace();}
+}
+	
+	public static ListaSudokus getListaSudokus() {
+		if (miListaSudokus == null) miListaSudokus = new ListaSudokus();
+		return miListaSudokus;
+	}
+	
+	public int[][] getLSudokus() {
+		return lSudokus.get(0);
 	}
 }
