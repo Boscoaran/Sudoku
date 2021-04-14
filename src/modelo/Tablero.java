f35eb4b57d89b96e5d55a68e5e84394a0e5be105
bosco <bosco@bosco-Inspiron-3793>
2021-04-14 18:38:58 +0200

    calcular candidatos

--- src/modelo/Tablero.java
+++ src/modelo/Tablero.java
@@ -1,5 +1,6 @@
 package modelo;
 
+import java.util.ArrayList;
 import java.util.Observable;
 
 @SuppressWarnings("deprecation")
@@ -87,6 +88,49 @@ public class Tablero extends Observable{
 		this.notifyObservers(arg);
 	}
 	
-	
-
+	public void calcularCandidatos(int x, int y) {
+		ArrayList<Integer> candidatos = new ArrayList<Integer>();
+		int pos = 0;
+		for(int n=0; n<9; n++){
+			candidatos.add(n+1);
+		}
+		for (int i=0; i<9; i++){
+			if (candidatos.contains((tablero[i][y]).getValor())) {
+				pos=candidatos.indexOf((tablero[i][y].getValor()));
+				candidatos.remove(pos);
+			}
+		}
+		for (int j=0; j<9; j++){
+			if (candidatos.contains((tablero[x][j]).getValor())) {
+				pos=candidatos.indexOf((tablero[x][j].getValor()));
+				candidatos.remove(pos);
+			}
+		}
+		int oX;
+		int oY;
+		if (x/3 <=1) {
+			oX=1;
+		}else if (x/3 <=2) {
+			oX=4;
+		}
+		else if (x/3 <=3) {
+			oX=7;
+		}
+		if (y/3 <=1) {
+			oY=1;
+		}else if (y/3 <=2) {
+			oY=4;
+		}
+		else if (y/3 <=3) {
+			oY=7;
+		}
+		for (int a=0; a<3; a++) {
+			for (int b=0; b<3; b++) {
+				if (candidatos.contains((tablero[x+a][y+b]).getValor())) {
+					pos=candidatos.indexOf((tablero[x+a][y+b].getValor()));
+					candidatos.remove(pos);
+				}
+			}
+		}
+	}
 }
